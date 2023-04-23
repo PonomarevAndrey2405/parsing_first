1537ff8feb9cf146cff97abeb7bb2347568eb251
PonomarevAndrey <damned01@yandex.ru>
2023-04-23 17:07:43 +0500

    parsing_first

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import json
 from time import sleep
 
 headers = Headers(browser='firefox', os='win').generate()
-#С помощью этой функции получаем url вакансий (всех)
+
+vacancy_parsed = []
+params = {
+    'text': 'python django flask'
+}
 def list_vacancy_url():
-    for count in range(0, 41):
+    for count in range(0, 1):
         url = f'https://spb.hh.ru/search/vacancy?text=python&area=1&area=2&page={count}&disableBrowserCache=true'
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'lxml')
@@ -15,32 +19,29 @@ def list_vacancy_url():
         for e in data:
             vacancy_url = e.find('a').get('href')
             yield vacancy_url
-#С помощью этой функции заходим в описание каждой вакансии, при помощи функции list_vacancy_url,
-#и достаём от туда название компании, зарплату и город
+
 def vacancy_item():
     for vacancy_url in list_vacancy_url():
-        response2 = requests.get(vacancy_url, headers=headers)
+        response2 = requests.get(vacancy_url, params=params, headers=headers)
         sleep(3)
         soup2 = BeautifulSoup(response2.text, 'lxml')
         data = soup2.find('div', class_='bloko-columns-row')
         vacancy_info = data.find('div', class_='g-user-content').text
         if 'Django' or 'Flask' in vacancy_info:
             company_name = data.find('span', class_='vacancy-company-name').text
-            salary = data.find('span', class_='bloko-header-section-2 bloko-header-section-2_lite')
-            if salary == None:
-                yield None
-            else:
-                yield salary.text
+            salary = data.find('span', class_='bloko-header-section-2 bloko-header-section-2_lite').text
             vacancy_data = data.find('div', class_='vacancy-company-redesigned')
-            city = vacancy_data.find('p').text
-            yield company_name, salary, city
+            city = vacancy_data.find('p')
+            vacancy_link = 'https://spb.hh.ru' + data.find('a', class_='bloko-button bloko-button_kind-success bloko-button_scale-large bloko-button_stretched')['href']
+            vacancy_parsed.append(
+                {
+                    'название компании': company_name,
+                    'зарплата': salary,
+                    'город': city,
+                    'ссылка': vacancy_link
+                }
+            )
 
-#Запись в файл тех вакансий, которые в описании имеют "Django" или "Flask"
-dictionary = {
-    company_name: vacancy_item(company_name),   #запись названия компании
-    salary: vacancy_item(salary),         #запись зарплаты
-    city: vacancy_item(city),           #запись города
-    vacancy_url: list_vacancy_url(vacancy_url) #запись url вакансии
-}
-with open('vacancy_info.json', 'w') as f:
-    json.dump(dictionary, f)
+if __name__ == '__main__':
+    with open('vacancy_info.json', 'w', encoding='utf-8') as f:
+        json.dump(vacancy_parsed, f)
